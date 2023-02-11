2203cc97d9788edefc89a90b205dad285f9b6e56
Claudio Vicenti <claudio.vicenti@hotmail.it>
2023-02-11 17:11:06 +0100

    primi passi

--- source/main.lua
+++ source/main.lua
@@ -1,12 +1,25 @@
 
 import "CoreLibs/graphics"
 
+--Imposta in ogni file dove ti serve delle constanti locali che richiamano le librerie
+--Questo incrementa le performance
+local pd <const> = playdate
+local gfx <const> = playdate.graphics
+
 --Dichiaro 2 varaibili locali, rispettivamente 'playerX da 200' e 'playerY da 120'
 local playerX, playerY = 200, 120
 local playerRadius = 10
+local playerSpeed = 1
 
 --Questa funzione viene chiamata ogni il refresh dello schermo
 --La console va a 30 FPS, quindi viene chiamata 30 volte al secondo
-function playdate.update()
-	playdate.graphics.fillCircleAtPoint(playerX, playerY, playerRadius)
+--Se la definizione di questa funzione non fosse presente, il gioco va in crash
+
+function pd.update()
+	gfx.clear()
+	--la funzione 'getCrankPosition' ritorna angolo e gradi, ma la convertiamo in raggio
+	local crackAngle = math.rad(pd.getCrankPosition())
+	playerX += math.sin(crackAngle) * playerSpeed
+	playerY -= math.cos(crackAngle) * playerSpeed
+	gfx.fillCircleAtPoint(playerX, playerY, playerRadius)
 end
